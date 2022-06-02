1b365f8da7c9311592f86e332d1888e152e37828
Beatriceanena <anenabeattrice@gmail.com>
2022-06-02 19:51:52 +0300

    loginActivity added

--- app/src/main/java/com/beatrice/workoutlog/SignupActivity.kt
+++ app/src/main/java/com/beatrice/workoutlog/SignupActivity.kt
@@ -1,12 +1,11 @@
 package com.beatrice.workoutlog
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-startActivity(Intent(this,LoginActivity::class.java))
+        setContentView(R.layout.activity_signup)
     }
 }
